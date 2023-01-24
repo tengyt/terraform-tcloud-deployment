30df5a936fc55a71e8a4b3530a8a78845016a367
tengyt <111709361+tengyt@users.noreply.github.com>
2023-01-24 16:20:28 -0600

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ca-central-1"
+}
+
+terraform {
+  cloud {
+    organization = "FusionIT"
+
+    workspaces {
+      name = "gh-action-demo-eva"
+    }
+  }
+}
