a02f8ff3772ab81e42060b3be6a1e5bfe5f3e2a4
tengyt <111709361+tengyt@users.noreply.github.com>
2023-01-24 19:09:51 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,48 @@
-resource "aws_ecs_cluster" "cluster" {
-  name = "${var.project}-${var.environment}"
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
+
+
+
+
+
+resource "aws_security_group" "Hannah_security" {
+  name        = "Hannah_security"
+  description = "security group using terraform"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-}
 
-resource "aws_ecs_cluster_capacity_providers" "terr-cloud" {
-  cluster_name = aws_ecs_cluster.cluster.name
-
-  capacity_providers = ["FARGATE"]
-
-  default_capacity_provider_strategy {
-    base              = 1
-    weight            = 100
-    capacity_provider = "FARGATE"
+ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-}
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Hannah_security"
+  }
+}  
