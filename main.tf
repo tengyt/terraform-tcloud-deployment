349c7d4755a26973d2916da85b1ffc64d54f06f5
tengyt <111709361+tengyt@users.noreply.github.com>
2023-01-25 20:39:55 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
-name = var.name_input
+module "tomcat" {
+source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+  name = var.name_input
 
   ami                    = var.my_ami
   instance_type          = "t2.micro"
