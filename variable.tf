06b5698032baa15157de66402d65c1244edd550e
tengyt <111709361+tengyt@users.noreply.github.com>
2023-01-25 20:27:01 -0600

    Create variable.tf
    added variable.tf

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "name_input" {
+    description = "instance name hannah"
+    type = string
+    default = "hannah"
+    }
+
+variable "region" {
+    description = "the aws region"
+    type = string
+    default = "us-east-1"
+    }
+
+variable "my_ami" {
+    description = "aws ami"
+    type = string
+    default = "ami-06640050dc3f556bb"
+    }
+variable "key" {
+    description = "aws key"
+    type = string
+    default = "key23"
+    }
