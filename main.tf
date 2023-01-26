3ca834a3cf14cd4bd299d3c7807854b4ff0ffe15
tengyt <111709361+tengyt@users.noreply.github.com>
2023-01-25 20:31:13 -0600

    Update main.tf
    added ec2 instance information

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
+name = var.name_input
 
+  ami                    = var.my_ami
+  instance_type          = "t2.micro"
+  key_name               = var.key
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
 
 
 
@@ -6,15 +16,6 @@
 resource "aws_security_group" "Hannah_security" {
   name        = "Hannah_security"
   description = "security group using terraform"
-
-  ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
 ingress {
     description      = "SSH"
@@ -25,15 +26,7 @@ ingress {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
+  
   egress {
     from_port        = 0
     to_port          = 0
